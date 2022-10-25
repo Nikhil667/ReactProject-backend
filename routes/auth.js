d8d5acfe29ac3431a6d2b20a9d46bb7e92afc001
Nikhil <33026376+Nikhil667@users.noreply.github.com>
2022-10-25 17:43:43 +0530

    third -commit
    Password Verification
    https://www.npmjs.com/package/jsonwebtoken
    https://www.npmjs.com/package/jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const JWT_SECRET = "maatujhesalaam";
+
+var jwt = require('jsonwebtoken');
+
+
 // create a user using post endpoint api/auth/createuser
 // /no login required
-
-
 router.post('/createuser',[
     body('email', "Enter Valid Email").isEmail(),
     body('name', "Enter Valid Name").isLength({ min: 3 }),
@@ -18,65 +23,37 @@ router.post('/createuser',[
     }
     //check whether the user with same email exists already
     try {
-      
-    
     let user = await User.findOne({email: req.body.email})
     if(user){
       return res.status(400).json({error : "Sorry a user with this email already exists"});
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    
+    //create a new user
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
     })
-      
-    res.json(user)  
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, JWT_SECRET)
+    //console.log(authToken)
+    res.json(authToken) 
+    
+    
   } catch (error) {
       console.log(error.message);
       res.status(500).send("Some error occurred");
   }
 
-      
-      
-      
-      
-      
-      //.then(user => res.json(user))
-      // .catch(err => {console.log(err)
-      // res.json({error: "Please enter unique value for email", message: err.message})})
-
-
-
-      
-    // User.create({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    //   }).then(user => res.json(user))
-    //   .catch(err => {console.log(err)
-    //   res.json({error: "Please enter unique value for email", message: err.message})})
 
 })
-
-// router.post('/', (req, res) => {
-
-//     console.log(req.body);
-//     //res.send("hh")
-
-//     const user = User(req.body);
-//     user.save();
-//     res.send(req.body)
-
-// })
-
-// router.get('/', (req, res) => {
-//     const obj = {
-//       name: "shanks"
-//     }
-    
-//       res.json(obj)
-//   })
-
 
 
 module.exports = router;
